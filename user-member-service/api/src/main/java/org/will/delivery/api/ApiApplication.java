2d3652199fa0715f19ee09201136ba3c6ec9a426
will_d <willd88@positivehotel.com>
2024-01-23 17:37:05 +0900

    multimodule 적용

--- user-member-service/api/src/main/java/org/will/delivery/api/ApiApplication.java
+++ user-member-service/api/src/main/java/org/will/delivery/api/ApiApplication.java
@@ -1,11 +1,11 @@
-package org.willd.userservice;
+package org.will.delivery.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class ApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
